7b87893c04b53619dd440b6da2f68eb3ccabdfe0
Abeer Hasan <Abeerhasan080@gmail.com>
2020-11-08 21:40:30 +0200

    Made Core Data manager and Tasks screnn

---
+++ MyTasks/MyTasks/ViewModel/AddTaskViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddTaskViewModel.swift
+//  MyTasks
+//
+//  Created by Mohammed Mohsin Sayed on 11/5/20.
+//  Copyright © 2020 Abeer Abbas Saber. All rights reserved.
+//
+
+import Foundation
+
+class AddTaskViewModel {
+    
+}
---
+++ MyTasks/MyTasks/Views/EventDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailsViewController.swift
+//  MyTasks
+//
+//  Created by Mohammed Mohsin Sayed on 11/5/20.
+//  Copyright © 2020 Abeer Abbas Saber. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
